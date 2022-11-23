54e560ce805ab67ceeb456d995a3a299b44a7058
unstoppablesahilsaifi <97735834+unstoppablesahilsaifi@users.noreply.github.com>
2022-11-23 17:36:43 +0530

    Update JSP_Directives.java

--- JSP_Directives.java
+++ JSP_Directives.java
@@ -1,1 +1,7 @@
-//Test
+// Directive tags (The jsp directives are messages that tells the web container how to translate a JSP page into the corresponding servlet.)
+1) Page Directive
+2) Include Directive
+3) Taglib Directive
+
+
+//Page Directive (BY this we can tell the direction vo jo hme pure page per apply krni h)
