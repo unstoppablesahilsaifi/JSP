8dad7fac69cefb42fbbbfb137fa3b714bcd988ae
unstoppablesahilsaifi <97735834+unstoppablesahilsaifi@users.noreply.github.com>
2022-12-06 15:56:08 +0530

    Update 6_Implicit_Objects.java

--- 6_Implicit_Objects.java
+++ 6_Implicit_Objects.java
@@ -2,3 +2,4 @@
 Implicit Objects are created during the translation phase of JSP to Servlet.
 These objects can be directly used in scriptlet that goes in the  service method.
 They are created by container automatically, and they can be accessed using objects.
+// For More-   https://www.javatpoint.com/jsp-implicit-objects
