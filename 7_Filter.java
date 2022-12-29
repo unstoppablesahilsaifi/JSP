861da7d7f52628f4ad46d8132677bedaa72537d5
unstoppablesahilsaifi <97735834+unstoppablesahilsaifi@users.noreply.github.com>
2022-12-29 13:50:25 +0530

    Update 7_Filter.java

--- 7_Filter.java
+++ 7_Filter.java
@@ -1,3 +1,6 @@
 // Java FILTER
-1st commit
-2nd commit
+Filters used for validation at the server side.
+Filter is an object that is invoked at the  preprocessing and post processing of a request.
+//Usage
+  Authentication and authorization of the request
+  input validation
